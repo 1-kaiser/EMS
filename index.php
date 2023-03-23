9ad295587be1e086342f08579542cdb0fc4a7b57
zZz <emmanuelpunay6906@gmail.com>
2023-03-23 19:11:48 +0800

    ongoing

--- index.php
+++ index.php
@@ -4,15 +4,65 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>EMS</title>
+    <title>EMs</title>
 
     <link rel="icon" href="./assets/img/configuration.ico" type="image/x-icon">
 
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;500&display=swap" rel="stylesheet">
+
     <link rel="stylesheet" href="./assets/css/bootstrap.min.css">
     <link rel="stylesheet" href="./assets/css/aos.css">
+    <link rel="stylesheet" href="./map_css/style.css">
 
 </head>
 <body>
+
+    <!-- user/admin -->
+    <div class="homepage">
+
+    <nav>
+        <div class="nav_container">
+            <div class="nav_title">
+                <h4>EMS</h4>
+            </div>
+            <div class="nav_items">
+                <a href="#">About Us</a>
+            </div>
+        </div>
+    </nav>
+
+    <div class="homepage_title">
+        <div class="homepage_text">
+
+            <h2 class="homepage_body_title">Employee Management System</h2>
+            <p class="homepage_body">A useful system in the business industry that can assist the human resources 
+            <br>department in managing their employees more efficiently. <br><br> login as:</p>
+
+            <div class="row">
+                <div class="col">
+                    <a href="">
+                        <img src="./assets/img/admin.png" alt="">
+                        <p>Admin</p>
+                    </a>
+                </div>
+                <div class="col">
+                    <a href="">
+                        <img src="./assets/img/user.png" alt="">
+                        <p class="user">User</p>
+                    </a>
+                </div>
+            </div>
+        </div>
+
+        <div class="homepage_hero">
+            <img src="./assets/img/hero.png" alt="">
+        </div>
+    </div>
+
+    </div>
+
     
     
 
