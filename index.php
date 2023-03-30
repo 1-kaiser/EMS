62582f5e7340e2d32e146bf5313ad414df8f3c19
KevinAyanokoji <kevin.goto25@gmail.com>
2023-03-30 13:27:57 +0800

    Update index.php

--- index.php
+++ index.php
@@ -99,8 +99,7 @@
         </div>
     </div>
 
-    <script src="./assets/js/bootstrap.bundle.min.js"></script>
-    <script src="./assets/js/aos.js"></script>
+    
     <script>
         AOS.init ({
             offset: 120,
